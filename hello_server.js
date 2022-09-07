fa19d595f9e8f0cd5fe9d931caf1da013b2abda1
Bari <ajibari@gmail.com>
2022-09-08 06:09:05 +0700

    add console.log when node is running and return event listener in end of the request

--- hello_server.js
+++ hello_server.js
@@ -8,16 +8,21 @@ const server = http.createServer(function(req, res){
 			body.push(chunk)
 		})
 
-		req.on('end', () => {
+		return req.on('end', () => {
 			const parsedBody = Buffer.concat(body).toString()
 			fs.appendFile('output.txt', parsedBody.split('=')[1] + '\r\n', (err) => {
-
+				if (err) {
+					//throw err; //will throw error and stop the event loop just like process.exit()
+					console.log(err);
+				}
 			})
+
+			res.statusCode = 302 //it's important to set statusCode to 302 to redirect user
+			res.setHeader('Location', '/')
+			return res.end()
 		})
 
-		res.statusCode = 302 //it's important to set statusCode to 302 to redirect user
-		res.setHeader('Location', '/')
-		return res.end()
+		
 	}
 	res.setHeader('Content-Type', 'text/html');
 	res.write('<html><head><title>My Page</title></head>');
@@ -28,4 +33,6 @@ const server = http.createServer(function(req, res){
 	return res.end();
 });
 
-server.listen(3000)
+server.listen(3000, () => {
+	console.log("Server is running on port 3000")
+})
